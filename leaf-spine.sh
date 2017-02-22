2c7057d8b8c82f232101d1957e637ba924933cb6
Chris Morgan <mihalis68@gmail.com>
2017-02-22 01:32:27 -0500

    Final corrections to initial version for correct configuration of base demo

--- leaf-spine.sh
+++ leaf-spine.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+#
+# Scripted version of the example leaf/spine setup given by Cumulus
+# for learning Cumulus VX networking
+#
+
 VBM=/usr/local/bin/VBoxManage
 CUMULUS_IMAGE=cumulus-linux-3.2.1-vx-amd64-1486153138.ac46c24zd00d13e.ova
 
@@ -105,7 +111,7 @@ for LEAF in 1 2; do
     fi
 done
 
-
+#set -x
 for SPINE in 1 2; do
 
     VMNAME="${VM_PREFIX}-spine${SPINE}"
@@ -149,10 +155,21 @@ function getvminfo {
     echo $IP    
 }
 
+SOCKETDIR=${HOME}/.ssh/sockets
+if [[ ! -d $SOCKETDIR ]]; then
+    mkdir ${SOCKETDIR}
+    chmod a+rwx ${SOCKETDIR}
+else
+    ls ${SOCKETDIR}
+fi
 
+SSH_COMMAND_OPTS="-o ControlMaster=auto -o ControlPath=${SOCKETDIR}/%r@%h-%p -o ControlPersist=600"
 SSHCOMMON="-q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o VerifyHostKeyDNS=no"
-SSHCMD="ssh ${SSHCOMMON}"
+SSHCMD="ssh $SSH_COMMAND_OPTS ${SSHCOMMON}"
+SCPCMD="scp $SSH_COMMAND_OPTS ${SSHCOMMON}"
 
+# Work out the IP address of this VM and then install an ssh key to
+# allow easy access from now on
 function preconfig_vm() {
     VMNAME="$1"
     check_vm_running "$VMNAME"
@@ -160,11 +177,12 @@ function preconfig_vm() {
 	vftrace "$VMNAME is running\n"
 	IP=`getvminfo "${VMNAME}"`
 	vftrace "Now configuring ${VNAME} ..."
-	cat ~/.ssh/id_rsa.pub | sshpass -p 'CumulusLinux!' $SSHCMD cumulus@${IP} 'umask 0077; mkdir -p .ssh; cat >> .ssh/authorized_keys && echo "Key copied"'
+	cat ~/.ssh/id_rsa.pub | sshpass -p 'CumulusLinux!' $SSHCMD cumulus@${IP} 'umask 0077; mkdir -p .ssh; cat >> .ssh/authorized_keys'
+	vftrace "Key copied\n"
     fi
 }
 
-
+# Get the VMs started
 for LEAF in 1 2; do
     VMNAME="${VM_PREFIX}-leaf${LEAF}"
     startVM "$VMNAME"
@@ -175,15 +193,46 @@ for SPINE in 1 2; do
 done
 
 sleep 20
-nmap -sn 10.0.1.0/24
+LOCALNET="10.0.1.0/24"
+vftrace "Updating arp for $LOCALNET ..." 
+nmap -sn $LOCALNET >/dev/null 2>&1
+vftrace "done\n"
+
+declare -a ADDRS
+
+for SPINE in 1 2; do
+    VMNAME="${VM_PREFIX}-spine${SPINE}"
+    preconfig_vm "${VMNAME}"
+    ADDRS["$SPINE"]="$IP"
+    ${SCPCMD} Quagga.conf.spine${SPINE} interfaces.spine${SPINE} daemons sudo cumulus@"${IP}":/tmp
+    echo "CumulusLinux!" | ${SSHCMD} -t cumulus@${IP} "sudo -S cp /tmp/sudo /etc/sudoers.d/cumulus "
+    ${SSHCMD} -t cumulus@"${IP}" "sudo cp /tmp/interfaces.spine${SPINE} /etc/network/interfaces"
+    ${SSHCMD} -t cumulus@"${IP}" "sudo cp /tmp/daemons /etc/quagga/daemons"
+    ${SSHCMD} -t cumulus@"${IP}" "sudo cp /tmp/Quagga.conf.spine${SPINE} /etc/network/Quagga.conf"
+    ${SSHCMD} -t cumulus@"${IP}" "sudo systemctl restart networking"
+    ${SSHCMD} -t cumulus@"${IP}" "sudo systemctl restart quagga.service"
+done
 
 for LEAF in 1 2; do
     VMNAME="${VM_PREFIX}-leaf${LEAF}"
     preconfig_vm "${VMNAME}"
-done
-for SPINE in 1 2; do
-    VMNAME="${VM_PREFIX}-spine${SPINE}"
-    preconfig_vm "${VMNAME}"
+    ADDRS[$LEAF+2]="$IP"
+    ${SCPCMD} Quagga.conf.leaf${LEAF} interfaces.leaf${LEAF} daemons sudo cumulus@"${IP}":/tmp
+    echo "CumulusLinux!" | ${SSHCMD} -t cumulus@${IP} "sudo -S cp /tmp/sudo /etc/sudoers.d/cumulus "
+    ${SSHCMD} -t cumulus@"${IP}" "sudo cp /tmp/interfaces.leaf${LEAF} /etc/network/interfaces"
+    ${SSHCMD} -t cumulus@"${IP}" "sudo cp /tmp/daemons /etc/quagga/daemons"
+    ${SSHCMD} -t cumulus@"${IP}" "sudo cp /tmp/Quagga.conf.leaf${LEAF} /etc/network/Quagga.conf"
+    ${SSHCMD} -t cumulus@"${IP}" "sudo systemctl restart networking"
+    ${SSHCMD} -t cumulus@"${IP}" "sudo systemctl restart quagga.service"
+    
 done
 
+vftrace "All VMs configured\n"
 
+# run tests
+IP1=${ADDRS[1]}
+IP2=${ADDRS[2]}
+IP3=${ADDRS[3]}
+IP4=${ADDRS[4]}
+vftrace "IPs : $IP1 $IP2 $IP3 $IP4\n"
+${SSHCMD} -t cumulus@"${IP1}" "ping -c1 $IP2; ping -c1 $IP3; ping -c1 $IP4"
